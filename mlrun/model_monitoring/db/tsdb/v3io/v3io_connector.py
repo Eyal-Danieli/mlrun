197c8f185e9eb6a34ff4bca6473451099846cc88
Eyal-Danieli <eyald@iguazio.com>
2025-07-01 17:17:26 +0300

    remove timestamp column from the groupby

--- mlrun/model_monitoring/db/tsdb/v3io/v3io_connector.py
+++ mlrun/model_monitoring/db/tsdb/v3io/v3io_connector.py
@@ -801,16 +801,67 @@ class V3IOTSDBConnector(TSDBConnector):
 
         return df_handler(df=df, metrics=metrics, project=self.project)
 
+    # @staticmethod
+    # def _get_sql_query(
+    #     *,
+    #     endpoint_id: str,
+    #     table_path: str,
+    #     name: str = mm_schemas.ResultData.RESULT_NAME,
+    #     metric_and_app_names: Optional[list[tuple[str, str]]] = None,
+    #     columns: Optional[list[str]] = None,
+    # ) -> str:
+    #     """Get the SQL query for the results/metrics table"""
+    #     if columns:
+    #         selection = ",".join(columns)
+    #     else:
+    #         selection = "*"
+    #
+    #     with StringIO() as query:
+    #         query.write(
+    #             f"SELECT {selection} FROM '{table_path}' "
+    #             f"WHERE {mm_schemas.WriterEvent.ENDPOINT_ID}='{endpoint_id}'"
+    #         )
+    #         if metric_and_app_names:
+    #             query.write(" AND (")
+    #
+    #             for i, (app_name, result_name) in enumerate(metric_and_app_names):
+    #                 sub_cond = (
+    #                     f"({mm_schemas.WriterEvent.APPLICATION_NAME}='{app_name}' "
+    #                     f"AND {name}='{result_name}')"
+    #                 )
+    #                 if i != 0:  # not first sub condition
+    #                     query.write(" OR ")
+    #                 query.write(sub_cond)
+    #
+    #             query.write(")")
+    #
+    #         query.write(";")
+    #         return query.getvalue()
+
+
     @staticmethod
     def _get_sql_query(
         *,
-        endpoint_id: str,
         table_path: str,
+        endpoint_id: Optional[str] = None,
+        application_names: Optional[list[str]] = None,
         name: str = mm_schemas.ResultData.RESULT_NAME,
         metric_and_app_names: Optional[list[tuple[str, str]]] = None,
         columns: Optional[list[str]] = None,
+        group_by_columns: Optional[list[str]] = None,
     ) -> str:
         """Get the SQL query for the results/metrics table"""
+
+        if metric_and_app_names and not endpoint_id:
+            raise mlrun.errors.MLRunInvalidArgumentError(
+                "If metric_and_app_names is provided, endpoint_id must also be provided"
+            )
+
+        if metric_and_app_names and application_names:
+            raise mlrun.errors.MLRunInvalidArgumentError(
+                "Cannot provide both metric_and_app_names and application_names"
+            )
+
         if columns:
             selection = ",".join(columns)
         else:
@@ -819,10 +870,17 @@ class V3IOTSDBConnector(TSDBConnector):
         with StringIO() as query:
             query.write(
                 f"SELECT {selection} FROM '{table_path}' "
-                f"WHERE {mm_schemas.WriterEvent.ENDPOINT_ID}='{endpoint_id}'"
+                # f"WHERE {mm_schemas.WriterEvent.ENDPOINT_ID}='{endpoint_id}'"
             )
+            if endpoint_id:
+                query.write(
+                    f" WHERE {mm_schemas.WriterEvent.ENDPOINT_ID}='{endpoint_id}'"
+                )
             if metric_and_app_names:
-                query.write(" AND (")
+                if endpoint_id:
+                    query.write(" AND (")
+                else:
+                    query.write(" WHERE (")
 
                 for i, (app_name, result_name) in enumerate(metric_and_app_names):
                     sub_cond = (
@@ -835,8 +893,28 @@ class V3IOTSDBConnector(TSDBConnector):
 
                 query.write(")")
 
+            if application_names:
+                if endpoint_id or metric_and_app_names:
+                    query.write(" AND (")
+                else:
+                    query.write(" WHERE (")
+                for i, app_name in enumerate(application_names):
+                    sub_cond = (
+                        f"{mm_schemas.WriterEvent.APPLICATION_NAME}='{app_name}'"
+                    )
+                    if i != 0:  # not first sub condition
+                        query.write(" OR ")
+                    query.write(sub_cond)
+
+            if group_by_columns:
+                query.write(" GROUP BY ")
+                query.write(",".join(group_by_columns))
+
+
             query.write(";")
             return query.getvalue()
+
+    # def _get_sql_query_for_processed_model_endpoints
 
     def read_predictions(
         self,
@@ -1275,7 +1353,17 @@ class V3IOTSDBConnector(TSDBConnector):
         end: Optional[Union[datetime, str]] = None,
         application_names: Optional[Union[str, list[str]]] = None,
     ) -> dict[str, int]:
-        raise NotImplementedError
+        now = mlrun.utils.datetime_now()
+        start = start or (now - timedelta(hours=24))
+        end = end or now
+        filter_query = ""
+        if application_names:
+            filter_query = self._generate_filter_query(
+                filter_key=mm_schemas.ApplicationEvent.APPLICATION_NAME,
+                filter_values=application_names,
+            )
+
+
 
     def calculate_latest_metrics(
         self,
