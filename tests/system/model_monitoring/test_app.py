7dd849fe4650b52b07cb8da83acf8a1a119c588a
Eyal-Danieli <eyald@iguazio.com>
2025-12-29 16:56:10 +0200

    optimize query data drift values for v3io

--- tests/system/model_monitoring/test_app.py
+++ tests/system/model_monitoring/test_app.py
@@ -320,9 +320,10 @@ class _V3IORecordsChecker:
 @TestMLRunSystemModelMonitoring.skip_test_if_env_not_configured
 @pytest.mark.enterprise
 class TestMonitoringAppFlow(TestMLRunSystemModelMonitoring, _V3IORecordsChecker):
-    project_name = "test-app-flow"
+    project_name = "test-app-flow-v2"
     # Set image to "<repo>/mlrun:<tag>" for local testing
-    image: typing.Optional[str] = None
+    # image: typing.Optional[str] = None
+    image = "artifactory.iguazeng.com:10557/eyald/mlrun:1.11.0"
     error_count = 10
 
     @classmethod
